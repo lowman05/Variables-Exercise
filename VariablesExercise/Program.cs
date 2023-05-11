873ad48ae0e0188e3014b1ae17e8fe7a4328a857
Daniel <dan.r.french@sbcglobal.net>
2023-05-11 10:53:03 -0500

    completed interpolation

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -2,9 +2,33 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Daniel";
+            int myAge = 38;
+            Console.WriteLine($"My name is {myName}, I am {myAge} years.");
+
+            char myGrade = 'A';
+            bool areWeHavingFun = true;
+            Console.WriteLine($"My grade in this class will be {myGrade}, and that is {areWeHavingFun}.");
+
+            double mySon = 4.5;
+            decimal pi = 3.14m;
+            Console.WriteLine($"My son is {mySon} years old, and pi is {pi}.");
         }
+
+    
+
+
+
+
+        
+
+
+
+        
+            
+
+        
     }
 }
